7f67f9d2d9fd32cd518cf1a86f116a8d705868b7
Bhoomikarwani <bhoomikarwani@gmail.com>
2025-09-08 22:24:50 +0530

    patterns

---
+++ lect8/upperLeft.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    // int n;
+    // printf("enter no. of rows:");
+    // scanf("%d",&n);
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n-i;j++){
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+    //OR
+    for(int i=0;i<3;i++){
+        for(int j=3;j>i;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
