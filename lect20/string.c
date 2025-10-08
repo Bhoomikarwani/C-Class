8b3fdf0308cc4742ae090f3a9e0e5e76b5b832ad
Bhoomikarwani <bhoomikarwani@gmail.com>
2025-10-09 00:26:45 +0530

    string

--- lect20/string.c
+++ lect20/string.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+
 int main(){
     
 //  char name[]="bhoomi";    // '\0' automatically add hota hai
@@ -11,11 +13,32 @@ int main(){
 //  printf("you enter: %s",name);
 //  return 0;
 
- char name[20];
- printf("enter your name:");
- gets(name);          // pure line le lega, space ke sath
- printf("you enter: %s",name);
- return 0;
+//  char name[20];
+//  printf("enter your name:");
+//  gets(name);          // pure line le lega, space ke sath
+//  printf("you enter: %s",name);
+//  return 0;
 
+//length of string 
+// char name[50];
+// printf("enter your name:");
+// gets(name);
+// int length=strlen(name);
+// printf("length of string = %d",length);
 
+//palindrome
+char name[50];
+printf("enter your name:");
+gets(name);
+int length=strlen(name);
+int istrue=1;
+for(int i=0;i<length/2;i++)
+ {
+   if(name[i]!=name[length-1-i])
+    {
+        istrue=0;
+        break;
+    }
+ }
+ istrue==1?printf("palindrome"):printf("not palindrome");
 }
