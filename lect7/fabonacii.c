d227062c18c7260d32d3c1c51a6ea72afa83ccb7
Bhoomi Karwani <bhoomi4737karwani@gmail.com>
2026-01-07 23:56:57 +0530

    fix error

--- lect7/fabonacii.c
+++ lect7/fabonacii.c
@@ -15,12 +15,25 @@ int main(){
     //using while loop
     i=1;
     while(i<=n){
-        printf("%d \t",a);;
+        printf("%d \t",a);
         next=a+b;
         a=b;
         b=next;
         i++;
     }
    
-    return 0;
+     return 0;
+
+     //wrong way
+//     printf("%d \t",a) ;
+//     printf("%d \t",b) ;
+//     for(int i=0; i<=n+1 ; i++){
+//         int next=a+b;
+//         printf("%d \t",next);
+//         a=b;
+//         b=next;
+//         i++;
+//     }
+//  return 0;
+
 }
